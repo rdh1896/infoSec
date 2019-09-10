22cafd79ae8cdb2837fba84b8ac9240a3b55ec18
Russell Harvey <39575139+rdh1896@users.noreply.github.com>
2019-09-09 21:23:02 -0400

    Add files via upload

--- Lab02/client.py
+++ Lab02/client.py
@@ -1,1 +1,27 @@
+import socket
+import sys
+
+def get():
+    pass
+
+def put():
+    pass
+
+def main():
+    command = sys.argv[1]
+    sourceFile = sys.argv[2]
+    destFile = sys.argv[3]
+    ip = input("Please enter the File Server's IP address: ")
+    port = 3306
+    severSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    severSocket.settimeout(0.5)
+    ip.connect((ip, port))
+    print("Connection Established.")
+    if command == "PUT":
+        pass
+    elif command == "GET":
+        pass
+    else:
+        print("Error: Invalid Command. Please use 'PUT' to place a file, or 'GET' to retrieve one")
+
 
--- Lab02/server.py
+++ Lab02/server.py
@@ -1,1 +1,36 @@
+import sys
+import socket
+import threading
 
+ip = "127.0.0.1"  # The IP address that our server will listen on
+port = 3306  # The port that our server will listen on
+
+
+def conn_handler(conn, address):
+    print(f"{address} has connected")
+
+
+def main():
+    # socket.socket() = socket class constructor
+    # socket.AF_INET = IPv4 will be used
+    # socket.SOCK_STREAM = TCP will be used (socket.SOCK_DGRAM = UDP)
+
+    srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # srv.bind() takes a tuple as an arg
+    # first element of the tuple is a string (IP or hostname)
+    # second element is an int (port number)
+    srv.bind((ip, port))
+
+    # Number of concurrent connections allowed
+    srv.listen(1)
+
+    while True:
+        # srv.accept() is a blocking call, waits for a connection
+        print("Waiting for a client...")
+        clientSock, address = srv.accept()
+        handler = threading.Thread(target=conn_handler, args=(clientSock, address))
+        handler.start()
+
+
+main()
