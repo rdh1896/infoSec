a614956f665b5ac97bee6cfeed6a92635b492263
Russell Harvey <39575139+rdh1896@users.noreply.github.com>
2019-09-10 20:37:05 -0400

    Add files via upload

--- Lab02/client.py
+++ Lab02/client.py
@@ -1,27 +1,69 @@
+"""
+Name: Russell Harvey <rdh1896@rit.edu>
+File: client.py
+Assignment: Lab 02
+"""
+
 import socket
 import sys
 
-def get():
-    pass
-
-def put():
-    pass
-
 def main():
-    command = sys.argv[1]
-    sourceFile = sys.argv[2]
-    destFile = sys.argv[3]
-    ip = input("Please enter the File Server's IP address: ")
-    port = 3306
-    severSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    severSocket.settimeout(0.5)
-    ip.connect((ip, port))
+    """
+    Establishes a connection with a server then asks user to enter a command.
+    User can enter:
+    'PUT ./filename.extension ./filename.extension'
+        PUT - implies placing a file on the server
+        First file name is the file you're placing on the server
+        Second file name is what the file will be named on the server after write
+    'GET ./filename.extension ./filename.extension'
+        GET - implies retrieving a file on the server
+        First file name is the file you're getting on the server
+        Second file name is what the file will be named on the client after write
+    After input, the request is sent to the server and is processed.
+    Transferred files will end up in the directory in which the respective Python programs
+    are running within.
+    :args: [hostname] [port]
+    :return: None
+    """
+    ip = sys.argv[1]
+    port = int(sys.argv[2])
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serverSocket.settimeout(0.5)
+    serverSocket.connect((ip, port))
     print("Connection Established.")
-    if command == "PUT":
-        pass
-    elif command == "GET":
-        pass
-    else:
-        print("Error: Invalid Command. Please use 'PUT' to place a file, or 'GET' to retrieve one")
+    while True:
+        data = input("Please enter a command: ")
+        command = data.split()
+        if command[0] == "PUT":
+            fileName = command[1]
+            try:
+                file = open(fileName, "rb")
+            except FileNotFoundError:
+                msg = "Error: No such file exists."
+                print(msg)
+                serverSocket.send(msg.encode())
+                break
+            serverSocket.send(command[0].encode())
+            file_data = file.read(1024)
+            serverSocket.send(file_data)
+            serverSocket.send(command[2].encode())
+            break
+        elif command[0] == "GET":
+            serverSocket.send(command[0].encode())
+            serverSocket.send(command[1].encode())
+            file_data = serverSocket.recv(1024)
+            if file_data.decode() == "Error: File not found.":
+                print("Error: File not found on server.")
+                break
+            fileName = command[2]
+            file = open(fileName, "wb")
+            file.write(file_data)
+            file.close()
+            break
+        else:
+            print("Error: Invalid Command. Please use 'PUT' to place a file, or 'GET' to retrieve one")
+            break
+
+main()
 
 
--- Lab02/server.py
+++ Lab02/server.py
@@ -1,36 +1,66 @@
+"""
+Name: Russell Harvey <rdh1896@rit.edu>
+File: server.py
+Assignment: Lab 02
+"""
+
 import sys
 import socket
 import threading
 
-ip = "127.0.0.1"  # The IP address that our server will listen on
-port = 3306  # The port that our server will listen on
-
+ip = "127.0.0.1" #IP address the server is listening on
+port = int(sys.argv[1]) #Port the server is listening on
 
 def conn_handler(conn, address):
+    """
+    Simply prints who has connected to the server.
+    (This is leftover code from the live code session in
+    class I decided to incorporate.)
+    :param conn: clientSock
+    :param address: address
+    :return:
+    """
     print(f"{address} has connected")
 
-
 def main():
-    # socket.socket() = socket class constructor
-    # socket.AF_INET = IPv4 will be used
-    # socket.SOCK_STREAM = TCP will be used (socket.SOCK_DGRAM = UDP)
-
+    """
+    Waits for a client to connect, then either gets or places files for the
+    client based on the command issued.
+    :return: None
+    """
     srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # srv.bind() takes a tuple as an arg
-    # first element of the tuple is a string (IP or hostname)
-    # second element is an int (port number)
     srv.bind((ip, port))
 
-    # Number of concurrent connections allowed
     srv.listen(1)
 
     while True:
-        # srv.accept() is a blocking call, waits for a connection
         print("Waiting for a client...")
         clientSock, address = srv.accept()
         handler = threading.Thread(target=conn_handler, args=(clientSock, address))
         handler.start()
+        data = clientSock.recv(1024).decode()
+        if data == "PUT":
+            info = clientSock.recv(1024)
+            if info == "Error: No such file exists.":
+                break
+            fileName = clientSock.recv(1024)
+            file = open(fileName, "wb")
+            file.write(info)
+            file.close()
+        else:
+            info = clientSock.recv(1024)
+            fileName = info.decode()
+            try:
+                file = open(fileName, "rb")
+            except FileNotFoundError:
+                msg = "Error: File not found."
+                clientSock.send(msg.encode())
+                break
+            file_data = file.read(1024)
+            clientSock.send(file_data)
+        clientSock.close()
+        break
 
 
 main()
